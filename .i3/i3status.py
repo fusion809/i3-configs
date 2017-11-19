cc8c7a7cfb3e8a956299160420d93f54b6f19c24
fusion809 <brentonhorne77@gmail.com>
2017-11-19 13:44:33 +1000

    Fixing temp monitor

--- .i3/i3status.py
+++ .i3/i3status.py
@@ -32,10 +32,7 @@ status.register("cpu_usage",
     format="{usage}%")
 
 status.register("temp",
-    format="{Core_0}C {Core_1}C {Core_2}C {Core_3}C",
-    hints={"markup": "pango"},
-    lm_sensors_enabled=True,
-    dynamic_color=True)
+    hints={"markup": "pango"})
 
 status.register("network", interface="wlo1", format_up="d{bytes_recv}KB/s u{bytes_sent}KB/s")
 
