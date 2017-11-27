a53595caaabd9b521e8fab458a9c60b17e1572c3
fusion809 <brentonhorne77@gmail.com>
2017-11-27 20:11:04 +1000

    Updating

--- .i3/i3status.py
+++ .i3/i3status.py
@@ -32,12 +32,12 @@ status.register("cpu_usage",
     format="{usage}%")
 
 status.register("temp",
-    format="{Core_0}°C {Core_1}°C {Core_2}°C {Core_3}°C",
+    format="{Core_0}°C {Core_1}°C",
     hints={"markup": "pango"},
     lm_sensors_enabled=True,
     dynamic_color=True)
 
-status.register("network", interface="wlo1", format_up="↓{bytes_recv}KB/s ↑{bytes_sent}KB/s")
+status.register("network", interface="wlp4s0", format_up="↓{bytes_recv}KB/s ↑{bytes_sent}KB/s")
 
 status.register("battery")
 
