841671153a4eb92ec596603682b1710156f42fd8
fusion809 <brentonhorne77@gmail.com>
2018-02-24 20:29:15 +1000

    Modernizing bar

--- .i3/i3status.py
+++ .i3/i3status.py
@@ -8,7 +8,7 @@ from i3pystatus import IntervalModule
 status = Status()
 
 status.register("clock",
-    format="%a %-d %b %y %r")
+    format="%a %-d/%m/%y %r")
 
 status.register("disk",
     path="/data",
@@ -41,6 +41,8 @@ status.register("network", interface="wlo1", format_up="↓{bytes_recv}KB/s ↑{
 
 status.register("battery")
 
+status.register("pulseaudio")
+
 status.register(
     'weather',
     format='{condition} {current_temp}{temp_unit}',
@@ -48,7 +50,6 @@ status.register(
     hints={'markup': 'pango'},
     backend=weathercom.Weathercom(
         location_code='ASXX0117:1:AS',
-       # locale='en-AU',
         units="metric",
     ))
 
@@ -57,14 +58,17 @@ status.register("uptime",
 
 status.register("shell",
     command="/usr/local/bin/vscoden",
+    color="#0FC0FF",
     interval=300)
 
 status.register("shell",
     command="/usr/local/bin/vimn",
+    color="#99AAFF",
     interval=300)
 
 status.register("shell",
     command="/usr/local/bin/swayn",
+    color="#FF00BB",
     interval=300)
 
 #status.register("shell",
@@ -86,7 +90,33 @@ status.register("shell",
 #    interval=300)
 
 status.register("shell",
+    command="/usr/local/bin/obss",
+    color="#00AF0F",
+    interval=300)
+
+status.register("shell",
+    command="/usr/local/bin/obsf",
+    color="#909C09",
+    interval=300)
+
+status.register("shell",
+    command="/usr/local/bin/obsd",
+    color="#390FCC",
+    interval=300)
+
+status.register("shell",
+    command="/usr/local/bin/oscn",
+    color="#AA0F39",
+    interval=300)
+
+status.register("shell",
+    command="/usr/local/bin/jucippn",
+    color="#F03000",
+    interval=300)
+
+status.register("shell",
     command="/usr/local/bin/atomn",
+    color="#FFAA00",
     interval=300)
 
 status.run()
