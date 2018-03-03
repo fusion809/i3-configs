df165879762bbdeaf09edcff7187e275ee9258f6
fusion809 <brentonhorne77@gmail.com>
2018-03-03 20:48:11 +1000

    OK->thumbs up in bar scripts

--- .i3/i3status.py
+++ .i3/i3status.py
@@ -67,6 +67,16 @@ status.register("shell",
     interval=300)
 
 status.register("shell",
+    command="/usr/local/bin/torn",
+    color="#CA30B2",
+    interval=300)
+
+status.register("shell",
+    command="/usr/local/bin/vimn",
+    color="#99AAFF",
+    interval=300)
+
+status.register("shell",
     command="/usr/local/bin/swayn",
     color="#FF00BB",
     interval=300)
