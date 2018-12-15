dcc3628fe829f2b856a116e1aebbca225cce6e45
Brenton Horne <brentonhorne77@gmail.com>
2018-12-16 01:49:17 +1000

    Let's monitor apps using check-updates-loop instead for now

--- .i3/i3status.sh
+++ .i3/i3status.sh
@@ -96,9 +96,9 @@ do
     ###################################### Status ############################################
     ##########################################################################################
  #  printf "%s\n" "$cond $temp °C | Up: $UP | ↓ $DOWN kB/s ↑ $UPL kB/s | CPU: $CPU% | RAM: $RAM/$TOT | Load: $LOAD | $TEMP0 | $TEMP1 | $TEMP2 | $TEMP3 | $TEMP4 | $DATE"
-    export PATH=$PATH:/data/GitHub/mine/scripts/nixos-scripts/usr/local/bin
-    printf "$(0adn) | $(avogadroln) | $(avogadron) | $(jmoln) | $(komodon) | $(libmsymn) | $(molen) | $(notepadqqn) | $(obsd) | $(obsf) | $(obss) | $(openbabeln) | $(openran) | $(openrabn) | $(openracn) | $(openracan) | $(openrad2n) | $(openradn) | $(openragn) | $(openra2n) | $(openramwn) | $(openraspn) | $(openrauran) | $(openrayn) | $(oscn) | $(rofin) | $(spglibn) | $(subln) | $(vimn) | $(vscoden) | $(zshn) | Up: $UP | CPU: $CPU% | RAM: $RAM/$TOT | Load: $LOAD | $DATE"
-#    echo "Up: $UP | CPU: $CPU% | RAM: $RAM/$TOT | Load: $LOAD | $DATE"
+ #   export PATH=$PATH:/data/GitHub/mine/scripts/nixos-scripts/usr/local/bin
+#    printf "%s\n" "$(0adn) | $(avogadroln) | $(avogadron) | $(jmoln) | $(komodon) | $(libmsymn) | $(molen) | $(notepadqqn) | $(obsd) | $(obsf) | $(obss) | $(openbabeln) | $(openran) | $(openrabn) | $(openracn) | $(openracan) | $(openrad2n) | $(openradn) | $(openragn) | $(openra2n) | $(openramwn) | $(openraspn) | $(openrauran) | $(openrayn) | $(oscn) | $(rofin) | $(spglibn) | $(subln) | $(vimn) | $(vscoden) | $(zshn) | Up: $UP | CPU: $CPU% | RAM: $RAM/$TOT | Load: $LOAD | $DATE"
+    echo "Up: $UP | CPU: $CPU% | RAM: $RAM/$TOT | Load: $LOAD | $DATE"
 
     #printf "%s\n" "Up: $UP | CPU: $CPU% | RAM: $RAM/$TOT | Load: $LOAD | $TEMP0 | $TEMP1 | $TEMP2 | $TEMP3 | $TEMP4 | $DATE"
 
