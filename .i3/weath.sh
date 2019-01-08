8253ac5c4632dd03e899e9d01db708daa22882ed
Brenton Horne <brentonhorne77@gmail.com>
2019-01-08 15:34:43 +1000

    mv C/C++ progs + scripts in .i3 to common-scripts

--- .i3/weath.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -cqO- http://www.weatherzone.com.au/qld/lower-burdekin/townsville | grep "icon-forecast-large weathericons" | cut -d '=' -f 3 | cut -d '"' -f 2 > cond.txt
-wget -cqO- http://www.weatherzone.com.au/qld/lower-burdekin/townsville | grep tempnow | cut -d '>' -f 2 | sed 's|</span||g' | sed 's|&deg;|°|g' > temp.txt
